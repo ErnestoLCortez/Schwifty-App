d201dcc1364fc756e6b1fb0c029849075507d22f
louie <louie@louies-iMac.local>
2016-12-22 13:20:26 -0800

    Initialize Pods and workspace

---
+++ Pods/Target Support Files/Pods-Schwifty Chat/Pods-Schwifty Chat-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Schwifty_ChatVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Schwifty_ChatVersionString[];
+
